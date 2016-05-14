2595943a95ac300fd73c7ae77811444a0a2d707a
Jack Jennings <jack@remote.gd>
2016-05-14 14:13:37 -0700

    Use with/as syntax for opening glif files

--- graver/__init__.py
+++ graver/__init__.py
@@ -7,7 +7,19 @@ from graver.reader.xml_reader import XMLReader
 __version__ = "0.0.1"
 
 
-def open(filepath):
+def read(filepath):
     reader = XMLReader.parse(os.path.abspath(filepath))
     version = reader.attribute('format')
     return GLIF(version=version, reader=reader)
+
+
+class open(object):
+
+    def __init__(self, filepath):
+        self.glif = read(filepath)
+
+    def __enter__(self):
+        return self.glif
+
+    def __exit__(self, type, value, trace):
+        pass
--- test/test_graver.py
+++ test/test_graver.py
@@ -5,25 +5,25 @@ from graver.glif import GLIF
 class TestGraver(object):
 
     def test_opens_glif1_file(self):
-        glif = graver.open('./test/fixtures/glif1.glif')
-        assert type(glif) is GLIF
-        assert glif.version is 1
-        assert glif.unicode.hex == "002E"
-        assert glif.advance.width == 268
+        with graver.open('./test/fixtures/glif1.glif') as glif:
+            assert type(glif) is GLIF
+            assert glif.version is 1
+            assert glif.unicode.hex == "002E"
+            assert glif.advance.width == 268
 
     def test_opens_glif2_file(self):
-        glif = graver.open('./test/fixtures/glif2.glif')
-        assert type(glif) is GLIF
-        assert glif.version is 2
-        assert glif.unicode.hex == "002E"
-        assert glif.advance.width == 268
-        assert glif.image.file_name == "period sketch.png"
-        assert glif.image.x_scale == 0.5
-        assert glif.image.y_scale == 0.6
-        assert len(glif.guidelines) is 1
-        assert glif.guidelines[0].y == -12
-        assert glif.guidelines[0].name == "overshoot"
-        assert len(glif.anchors) is 1
-        assert glif.anchors[0].x == 74
-        assert glif.anchors[0].y == 197
-        assert glif.anchors[0].name == "top"
+        with graver.open('./test/fixtures/glif2.glif') as glif:
+            assert type(glif) is GLIF
+            assert glif.version is 2
+            assert glif.unicode.hex == "002E"
+            assert glif.advance.width == 268
+            assert glif.image.file_name == "period sketch.png"
+            assert glif.image.x_scale == 0.5
+            assert glif.image.y_scale == 0.6
+            assert len(glif.guidelines) is 1
+            assert glif.guidelines[0].y == -12
+            assert glif.guidelines[0].name == "overshoot"
+            assert len(glif.anchors) is 1
+            assert glif.anchors[0].x == 74
+            assert glif.anchors[0].y == 197
+            assert glif.anchors[0].name == "top"
